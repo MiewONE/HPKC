e3e6b2c032c84c3246d9c44400a38c3acbd644dd
Miewone <dlsrk489@gmail.com>
2021-06-28 19:13:13 +0900

    passport session 관리를 해줘야 로그인이 완료됨.

--- controller/KakaoLoginController.js
+++ controller/KakaoLoginController.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const kakaoStrategy = require('passport-kakao').Strategy
 const express = require('express');
+const session = require('express-session')
 const fs = require('fs')
 
 const router = express.Router();
@@ -9,22 +10,41 @@ const router = express.Router();
 const key = fs.readFileSync('./key/kakaoKey',{
     encoding :"utf-8"
 });
+router.use(session({
+    secret:'test',
+    resave:true,
+    saveUninitialized:true
+}))
+router.use(passport.initialize())
+router.use(passport.session())
 passport.use('kakao-login',new kakaoStrategy({
     clientID:key,
-    callbackURL:'http://localhost:3045/login/kakao/oauth'
+    callbackURL:'http://localhost:3045/login/kakao/callback'
 }, async(accessToken,refreshToken,profile,done) => {
     console.log(accessToken);
     console.log(profile);
-    done(null, {accessToken,profile});
+    // return done(null,{accessToken,profile});
+    if(!profile)
+    {
+        done(null,undefined,"login fail");
+    }
+    done(null,profile);
 }));
 
-router.get('/kakao/oauth',passport.authenticate('kakao-login'));
+router.get('/kakao/oauth',passport.authenticate('kakao-login'),(req,res,next) => {
+    console.log(req);
+    res.end();
+});
+
 
 router.get('/kakao/callback',passport.authenticate('kakao-login', {
-    failureRedirect: '/',
+    failureRedirect: '/error',
 }),(req,res) => {
+
+    console.log("login Success");
     console.log(req);
     res.redirect('/');
 })
+
 
 module.exports = router;
